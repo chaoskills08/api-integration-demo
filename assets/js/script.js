525a91d02815e57b52c7c4e324691eed893a33f8
Nicholas Bales <nickb96@icloud.com>
2022-08-15 12:55:46 -0400

    made small changes to js fetch code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,21 @@
 var apiUrl = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=0A7CA6E07B05C1D631D0A8694267AE6D&steamids=76561198125798243";
 
-function getApi(apiUrl) {
+
+
+function getMeData() {
   fetch(apiUrl, {
-    method: 'GET',
-    mode: 'no-cors',
+    credentionals: 'same-origin',
+    mode: "cors",
+    header: {
+      'Content-Type': 'application/json'
+    }
   })
-    .then(function (response) {
-      console.log(response)
+    .then(function (r) {
+      console.log(r)
     })
-    .then(function (data) {
-      console.log(data);
+    .then(function (d) {
+      console.log(d)
     })
 }
-getApi(apiUrl);
+
+getMeData()

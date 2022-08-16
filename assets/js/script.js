2741c32ec4aa5fc163753c9d40f0c8822a598075
Jacob Karla <jacob.l.karla@gmail.com>
2022-08-16 10:16:48 -0400

    Added my code to branch outside of class repo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 var apiUrl = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=0A7CA6E07B05C1D631D0A8694267AE6D&steamids=76561198125798243";
-
+var ingredientsUrl = 'https://api.spoonacular.com/recipes/716429/information?includeNutrition=false&apiKey=ebbc0b7a9b7c4b61b20f0c5356d4334c';
+var instructionsUrl ="https://api.spoonacular.com/recipes/324694/analyzedInstructions?apiKey=ebbc0b7a9b7c4b61b20f0c5356d4334c" ;
+var img =document.getElementById("placeholder")
+var responseText = document.getElementById('response-text');
+var id = 0 //this is going to be set to whatever the logic is going to be from the game data.
 
 
 function getMeData() {
@@ -19,3 +23,33 @@ function getMeData() {
 }
 
 getMeData()
+
+
+function getApiIngredients(ingredientsUrl) {
+  fetch(ingredientsUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    console.log(data.title);
+    img.src=data.image;
+    for(var i=0;i<data.extendedIngredients.length;i++){
+        console.log(data.extendedIngredients[i].name);
+    }
+  })
+}
+getApiIngredients(ingredientsUrl);
+
+
+function getApi(instructionsUrl) { //sometimes this is not properly matched with an ID, should make logic to display that that has occured, and link to the actual website that the info was taken from. //
+    fetch(instructionsUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      
+    })
+  }
+getApi(instructionsUrl);

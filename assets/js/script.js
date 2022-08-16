3ba8cd9d4e22c029292dd134e6df5f71de6317f5
Jacob Karla <jacob.l.karla@gmail.com>
2022-08-16 13:43:47 -0400

    created functionality that puts items to the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
-var id = 716429 //this is going to be set to whatever the logic is going to be from the game data.
+var id = 324694 //this is going to be set to whatever the logic is going to be from the game data.
 var ingredientsUrl = 'https://api.spoonacular.com/recipes/'+id+'/information?includeNutrition=false&apiKey=ebbc0b7a9b7c4b61b20f0c5356d4334c';
 var instructionsUrl ="https://api.spoonacular.com/recipes/"+id+"/analyzedInstructions?apiKey=ebbc0b7a9b7c4b61b20f0c5356d4334c" ;
 var img =document.getElementById("placeholder")
 var responseText = document.getElementById('response-text');
 var buttonName=document.getElementById('buttonName');
+var issueContainer=document.getElementsByClassName("container");
 
 
 function getApiIngredients(ingredientsUrl) {
@@ -16,7 +17,9 @@ function getApiIngredients(ingredientsUrl) {
     console.log(data.title);
     img.src=data.image;
     for(var i=0;i<data.extendedIngredients.length;i++){
-        console.log(data.extendedIngredients[i].name);
+        var ingredientName = document.createElement('li');
+        ingredientName.textContent=data.extendedIngredients[i].name;
+        buttonName.append(ingredientName);
     }
   })
 }
@@ -28,6 +31,12 @@ function getApi(instructionsUrl) { //sometimes this is not properly matched with
     })
     .then(function (data) {
       console.log(data);
+      for(var i=0;i<data[0].steps.length;i++){
+        console.log(data[0].steps[i].step);
+        var stepName = document.createElement('li');
+        stepName.textContent=data[0].steps[i].step
+        buttonName.append(stepName);
+      }
       
     })
   }
@@ -36,5 +45,4 @@ buttonName.addEventListener("click",function(){
   console.log("This button works");
    getApi(instructionsUrl);
    getApiIngredients(ingredientsUrl);    //will display data, will probably also run the logic from converting value of game to that of a food" 
-
 })

16d76d4fab759601ee291709eeee9b723a19ede0
Jacob Karla <jacob.l.karla@gmail.com>
2022-08-17 13:28:13 -0400

    modified functions to only require 1 function, able to collect all data. Able to append all information to the page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-var gameId =0
-var id = 324694 //this is going to be set to whatever the logic is going to be from the game data.
-var ingredientsUrl = 'https://api.spoonacular.com/recipes/'+id+'/information?includeNutrition=false&apiKey=ebbc0b7a9b7c4b61b20f0c5356d4334c';
-var instructionsUrl ="https://api.spoonacular.com/recipes/"+id+"/analyzedInstructions?apiKey=ebbc0b7a9b7c4b61b20f0c5356d4334c" ;
+var gameId =0;
+var id = '0';//this is going to be set to whatever the logic is going to be from the game data.
+var ingredientsUrl = 'https://api.spoonacular.com/recipes/random?apiKey=ebbc0b7a9b7c4b61b20f0c5356d4334c&cuisine='+id;
+// var instructionsUrl ="https://api.spoonacular.com/recipes/"+id+"/analyzedInstructions?apiKey=ebbc0b7a9b7c4b61b20f0c5356d4334c" ;
 var img =document.getElementById("placeholder")
 var responseText = document.getElementById('response-text');
-var buttonName=document.getElementById('buttonName');
+var buttonName=document.getElementById('genBtn');
 var ingredients= document.getElementById("ingredientsList");
 var recipeSteps = document.getElementById("steps");
 
@@ -16,39 +16,87 @@ function getApiIngredients(ingredientsUrl) {
   })
   .then(function (data) {
     console.log(data);
-    console.log(data.title);
-    img.src=data.image;
-    for(var i=0;i<data.extendedIngredients.length;i++){
+    console.log(data.recipes[0].title);
+    var img1 =document.createElement("img");
+    img1.setAttribute('src',data.recipes[0].image);
+    steps.append(img1);
+    for(var i=0;i<data.recipes[0].extendedIngredients.length;i++){
         var ingredientName = document.createElement('li');
-        ingredientName.textContent=data.extendedIngredients[i].name;
+        ingredientName.textContent=data.recipes[0].extendedIngredients[i].original;
         ingredients.append(ingredientName);
+    for(var j=0;i<data.recipes[0].analyzedInstructions[0].steps.length;i++){
+        var stepName = document.createElement('li');
+        stepName.textContent=data.recipes[0].analyzedInstructions[0].steps[i].step
+        steps.append(stepName);
+    }
     }
   })
 }
 
-function getApi(instructionsUrl) { //sometimes this is not properly matched with an ID, should make logic to display that that has occured, and link to the actual website that the info was taken from. //
-    fetch(instructionsUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      for(var i=0;i<data[0].steps.length;i++){
-        console.log(data[0].steps[i].step);
-        var stepName = document.createElement('li');
-        stepName.textContent=data[0].steps[i].step
-        recipeSteps.append(stepName);
-      }
-      
-    })
+
+function idLogic(gameId) {
+   
+  if(gameId===1){
+    id='Middle_Eastern';
   }
+  else if(gameId===2){
+    id = 'Eastern European';
+  }
+  else if(gameId===3){
+    id='American';
+  }
+  else if(gameId===4){
+    id='Japanese';
+  }
+  else if(gameId===5){
+    id='British';
+  }
+}
+
+// function getApi(instructionsUrl) { //sometimes this is not properly matched with an ID, should make logic to display that that has occured, and link to the actual website that the info was taken from. //
+//     fetch(instructionsUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       for(var i=0;i<data[0].steps.length;i++){
+//         console.log(data[0].steps[i].step);
+//         var stepName = document.createElement('li');
+//         stepName.textContent=data[0].steps[i].step
+//         recipeSteps.append(stepName);
+//       }
+      
+//     })
+//   }
 
   function idLogic(gameId) {
-     id=gameId*34796
+   
+    if(gameId===1){
+      id='Middle_Eastern';
+    }
+    else if(gameId===2){
+      id = 'Eastern_European';
+    }
+    else if(gameId===3){
+      id='American';
+    }
+    else if(gameId===4){
+      id='Japanese';
+    }
+    else if(gameId===5){
+      id='British';
+    }
   }
+
+//   Metal Gear Solid 4 1  Middle Eastern
+// Battlefield: Bad Company 2  Eastern European
+// Burnout Paradise 3  American
+// Professor Layton and the Curious Village 4 Japanese 
+// Condemned 2: Bloodshot 5 British
 
 buttonName.addEventListener("click",function(){
   console.log("This button works");
-   getApi(instructionsUrl);
+  //  getApi(instructionsUrl);
    getApiIngredients(ingredientsUrl);    //will display data, will probably also run the logic from converting value of game to that of a food" 
 })

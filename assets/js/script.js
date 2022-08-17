8a6cf00d8abf3939a0dbabda282e9281fc1e644b
Nicholas Bales <nickb96@icloud.com>
2022-08-17 11:22:25 -0400

    made logic changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,16 @@
+var ingredientsUrl = 'https://api.spoonacular.com/recipes/716429/information?includeNutrition=false&apiKey=ebbc0b7a9b7c4b61b20f0c5356d4334c';
+var instructionsUrl = "https://api.spoonacular.com/recipes/324694/analyzedInstructions?apiKey=ebbc0b7a9b7c4b61b20f0c5356d4334c";
+var img = document.getElementById("placeholder")
+var responseText = document.getElementById('response-text');
+var id = 0 //this is going to be set to whatever the logic is going to be from the game data.
 var apiUrl = 'https:russelldev-cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/games/?api_key=aae6cead4d664ca28d5080355fbaefc5085d2381&format=json'
+var reviewsUrl = 'https:russelldev-cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/reviews/?api_key=aae6cead4d664ca28d5080355fbaefc5085d2381&filter=score:5&sort=score:asc&format=json&limit=5'
+var gameFetch = document.getElementById('gameFetch');
+var gameBtn = document.getElementById('btnId');
 
 
 
-function getMeData() {
+function getGameData() {
   fetch(apiUrl, {
     method: 'GET',
     header: {
@@ -18,18 +26,52 @@ function getMeData() {
     })
     .then(function (data) {
       console.log(data)
-      var filterData = data.results.filter(function (game) {
-        return game.aliases !== null
-
-      })
-      console.log(filterData)
+      // var filterData = data.results.filter(function (game) {
+      //   return game.aliases !== null
+      // })
+      // console.log(filterData)
     })
 }
-var ingredientsUrl = 'https://api.spoonacular.com/recipes/716429/information?includeNutrition=false&apiKey=ebbc0b7a9b7c4b61b20f0c5356d4334c';
-var instructionsUrl = "https://api.spoonacular.com/recipes/324694/analyzedInstructions?apiKey=ebbc0b7a9b7c4b61b20f0c5356d4334c";
-var img = document.getElementById("placeholder")
-var responseText = document.getElementById('response-text');
-var id = 0 //this is going to be set to whatever the logic is going to be from the game data.
+// getGameData()
+
+function getReviews() {
+  fetch(reviewsUrl, {
+    method: 'GET',
+    header: {
+      'Content-Type': 'application/json'
+    },
+    format: 'json',
+  })
+
+    .then(function (response) {
+      console.log(response)
+      return response.json()
+
+    })
+    .then(function (data) {
+      console.log(data)
+      var random = Math.floor(Math.random() * data.results.length)
+      console.log(random)
+      if (random === 0) {
+        console.log('good')
+      }
+      // if (data.results[0].game.name === "Metal Gear Solid 4: Guns of the Patriots") {
+      //   console.log('good')
+
+      // if (button0 === true) {
+      // createElement h2(game name), p(description)
+
+      //var game = event.target.getAttribute = "game1"
+      // if (game === 'game1') {
+      // 
+      // }
+    }
+
+    )
+}
+
+
+
 
 function getApiIngredients(ingredientsUrl) {
   fetch(ingredientsUrl)
@@ -45,7 +87,7 @@ function getApiIngredients(ingredientsUrl) {
       }
     })
 }
-getApiIngredients(ingredientsUrl);
+// getApiIngredients(ingredientsUrl);
 
 
 function getApi(instructionsUrl) { //sometimes this is not properly matched with an ID, should make logic to display that that has occured, and link to the actual website that the info was taken from. //
@@ -58,4 +100,5 @@ function getApi(instructionsUrl) { //sometimes this is not properly matched with
 
     })
 }
-getApi(instructionsUrl)
+// getApi(instructionsUrl)
+gameBtn.addEventListener('click', getReviews)

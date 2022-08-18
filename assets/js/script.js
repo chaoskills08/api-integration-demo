dbe61fab35e9ddc504b59df5f8f00646e91d39ed
Jacob Karla <jacob.l.karla@gmail.com>
2022-08-18 11:04:27 -0400

    changed my code to match existing container names, finished local storage functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var gameId =0;
-var id = '0';//this is going to be set to whatever the logic is going to be from the game data.
+//this is going to be set to whatever the logic is going to be from the game data.
 var ingredientsUrl = 'https://api.spoonacular.com/recipes/random?apiKey=ebbc0b7a9b7c4b61b20f0c5356d4334c&cuisine='+id;
 var img =document.getElementById("placeholder")
 var responseText = document.getElementById('response-text');
@@ -13,6 +13,9 @@ var reviewsUrl = 'https:russelldev-cors-anywhere.herokuapp.com/https://www.giant
 var gameFetch = document.getElementById('gameFetch');
 var gameBtn = document.getElementsByClassName("gameButtons");
 var gameInfo = document.getElementById("gameInfo");
+var titleFoodEl=document.getElementById("titleFood");
+var ingredientsListEl=document.getElementById("ingList");
+var stepListEl=document.getElementById("stepList");
 var savedData =[];
 // function getGameData() {
 //   fetch(apiUrl, {
@@ -64,12 +67,8 @@ function getReviews() {
 }
 
 var gameId = 0
-var id = 324694 //this is going to be set to whatever the logic is going to be from the game data.
-var img = document.getElementById("placeholder")
-var responseText = document.getElementById('response-text');
-var buttonName = document.getElementById('buttonName');
-var ingredients = document.getElementById("ingredientsList");
-var recipeSteps = document.getElementById("steps");
+//this is going to be set to whatever the logic is going to be from the game data.
+
 
 
 function getApiIngredients(ingredientsUrl) {
@@ -78,21 +77,22 @@ function getApiIngredients(ingredientsUrl) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
-    console.log(data.recipes[0].title);
     var name =data.recipes[0].title;
-    steps.append(name);
-    var img1 =document.createElement("img");
-    img1.setAttribute('src',data.recipes[0].image);
-    steps.append(img1);
+    titleFoodEl.textContent=name;
+    var img = document.createElement('img');
+    img.setAttribute('id','foodImg');
+    document.getElementById('foodImg').width='350';
+    document.getElementById('foodImg').height='300';
+    img.setAttribute('src',data.recipes[0].image);
+    titleFoodEl.appendChild(img);
     for(var i=0;i<data.recipes[0].extendedIngredients.length;i++){
         var ingredientName = document.createElement('li');
         ingredientName.textContent=data.recipes[0].extendedIngredients[i].original;
-        ingredients.append(ingredientName);
+        ingredientsListEl.append(ingredientName);
     for(var j=0;j<data.recipes[0].analyzedInstructions[0].steps.length;j++){
         var stepName = document.createElement('li');
         stepName.textContent=data.recipes[0].analyzedInstructions[0].steps[j].step
-        steps.append(stepName);
+        stepListEl.append(stepName);
     }
     }
   })
@@ -118,22 +118,6 @@ function idLogic(gameId) {
   }
 }
 
-// function getApi(instructionsUrl) { //sometimes this is not properly matched with an ID, should make logic to display that that has occured, and link to the actual website that the info was taken from. //
-//     fetch(instructionsUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//       for(var i=0;i<data[0].steps.length;i++){
-//         console.log(data[0].steps[i].step);
-//         var stepName = document.createElement('li');
-//         stepName.textContent=data[0].steps[i].step
-//         recipeSteps.append(stepName);
-//       }
-      
-//     })
-//   }
 
 
 //   Metal Gear Solid 4 1  Middle Eastern
@@ -161,49 +145,15 @@ buttonName.addEventListener("click",function(){
       }
     })
 
-// getApiIngredients(ingredientsUrl);
 
 
-// function getApi(instructionsUrl) { //sometimes this is not properly matched with an ID, should make logic to display that that has occured, and link to the actual website that the info was taken from. //
-//   fetch(instructionsUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//       console.log(data.title);
-//       img.src = data.image;
-//       for (var i = 0; i < data.extendedIngredients.length; i++) {
-//         var ingredientName = document.createElement('li');
-//         ingredientName.textContent = data.extendedIngredients[i].name;
-//         ingredients.append(ingredientName);
-//       }
-//     })
-// }
-
-// function getApi(instructionsUrl) { //sometimes this is not properly matched with an ID, should make logic to display that that has occured, and link to the actual website that the info was taken from. //
-//   fetch(instructionsUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//       for (var i = 0; i < data[0].steps.length; i++) {
-//         console.log(data[0].steps[i].step);
-//         var stepName = document.createElement('li');
-//         stepName.textContent = data[0].steps[i].step
-//         recipeSteps.append(stepName);
-//       }
-
-//     })
-// }
-// getApi(instructionsUrl);
 function saveData(){
   savedData.push(data.recipes[0].title);
   localStorage.setItem("savedData",JSON.stringify(savedData));
 }
 function renderMessage(){
-  
+  var showData=JSON.parse(localStorage.getItem("savedData"))
+
 }
 
 

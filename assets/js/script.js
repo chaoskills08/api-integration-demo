3f98a53777de2ff6576d880b69fcfb7e0b7df629
Jacob Karla <jacob.l.karla@gmail.com>
2022-08-18 10:23:31 -0400

    added local storage saving

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,7 @@ var reviewsUrl = 'https:russelldev-cors-anywhere.herokuapp.com/https://www.giant
 var gameFetch = document.getElementById('gameFetch');
 var gameBtn = document.getElementsByClassName("gameButtons");
 var gameInfo = document.getElementById("gameInfo");
-var gameId = 0
-
-
+var savedData =[];
 // function getGameData() {
 //   fetch(apiUrl, {
 //     method: 'GET',
@@ -63,11 +61,7 @@ function getReviews() {
         gameInfo.appendChild(descCreate);
       }
     })
-
-
-
 }
-
 
 var gameId = 0
 var id = 324694 //this is going to be set to whatever the logic is going to be from the game data.
@@ -170,40 +164,47 @@ buttonName.addEventListener("click",function(){
 // getApiIngredients(ingredientsUrl);
 
 
-function getApi(instructionsUrl) { //sometimes this is not properly matched with an ID, should make logic to display that that has occured, and link to the actual website that the info was taken from. //
-  fetch(instructionsUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      console.log(data.title);
-      img.src = data.image;
-      for (var i = 0; i < data.extendedIngredients.length; i++) {
-        var ingredientName = document.createElement('li');
-        ingredientName.textContent = data.extendedIngredients[i].name;
-        ingredients.append(ingredientName);
-      }
-    })
-}
+// function getApi(instructionsUrl) { //sometimes this is not properly matched with an ID, should make logic to display that that has occured, and link to the actual website that the info was taken from. //
+//   fetch(instructionsUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       console.log(data.title);
+//       img.src = data.image;
+//       for (var i = 0; i < data.extendedIngredients.length; i++) {
+//         var ingredientName = document.createElement('li');
+//         ingredientName.textContent = data.extendedIngredients[i].name;
+//         ingredients.append(ingredientName);
+//       }
+//     })
+// }
 
-function getApi(instructionsUrl) { //sometimes this is not properly matched with an ID, should make logic to display that that has occured, and link to the actual website that the info was taken from. //
-  fetch(instructionsUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      for (var i = 0; i < data[0].steps.length; i++) {
-        console.log(data[0].steps[i].step);
-        var stepName = document.createElement('li');
-        stepName.textContent = data[0].steps[i].step
-        recipeSteps.append(stepName);
-      }
+// function getApi(instructionsUrl) { //sometimes this is not properly matched with an ID, should make logic to display that that has occured, and link to the actual website that the info was taken from. //
+//   fetch(instructionsUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       for (var i = 0; i < data[0].steps.length; i++) {
+//         console.log(data[0].steps[i].step);
+//         var stepName = document.createElement('li');
+//         stepName.textContent = data[0].steps[i].step
+//         recipeSteps.append(stepName);
+//       }
 
-    })
-}
+//     })
+// }
 // getApi(instructionsUrl);
+function saveData(){
+  savedData.push(data.recipes[0].title);
+  localStorage.setItem("savedData",JSON.stringify(savedData));
+}
+function renderMessage(){
+  
+}
 
 
 for (let i = 0; i <= gameBtn.length; i++) {
